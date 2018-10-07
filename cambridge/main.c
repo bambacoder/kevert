8d7a7f0cb3af09a886d930d2a37bcd9b5ec20e19
bambacoder <bambagamer@gmail.com>
2018-10-07 19:26:17 +0200

    kezdtem, semmi

--- cambridge/main.c
+++ cambridge/main.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 int main()
 {
-    char szoveg[1000]={0};
+    char szoveg[50]={0};
+    scanf ("%s",szoveg);
+    char szo[50]={0};
+    int i= 0;
+    int j= 0;
+    char kevert[50]={0};
+        while (szoveg[j]!= '\0')
+        {
+            szo[i]= szoveg[j];
+            i++;
+            j++;
+        }
+        for (int k=1; k<strlen(szo); k++)
+        {
 
-    printf("Hello world!\n");
+        }
+        printf("%s\n",szo) ;
+
     return 0;
 }
